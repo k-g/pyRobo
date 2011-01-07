596a70de95ca5d32a1bd3291f2ae68e9181ac032
KG <kmgreen@vt.edu>
2011-01-07 14:54:21 -0500

    Cleaned up test.py (main), and removed old commented code from util_functions.py's rangeConvert function
    Signed-off-by: KG <kmgreen@vt.edu>

--- test.py
+++ test.py
@@ -14,272 +14,9 @@ from polar_plot import *            #polar plot
 from serial_threads import *
 
 #global variables
-telemetryPause = 0;
 t_thread = 0
 g_thread = 0
 
-# def enumerateSerialPorts(announce=1):
-#     """
-#     Will search for the first 256 ports and return the ports
-#     """
-#     port_counter = 0
-#     port_list    = []
-#     while port_counter < 256:
-#         try:
-#             s = serial.Serial(port_counter)
-#             port_list.append(s.portstr)
-#             if announce:            
-#                 print("Found port %s" % (s.portstr));      
-#         except serial.SerialException:
-#             pass
-#         finally:
-#             port_counter += 1;
-            
-#     return port_list
-    
-
-
-# def serialMonitor(serial_obj, alert_function):
-#     """
-#     The actual serial Thread
-#     """
-#     global isAlive
-#     data=""
-#     dataList = deque([0,0,0,0,0,0,0,0,0,0,0,0])
-#     sig = SigWrapper(alert_function)
-    
-#     while isAlive:
-#         try:
-#             the_new_val    =     serial_obj.read(1)
-#             data = data +      the_new_val        # read one, blocking
-#             dataList.append(the_new_val)  
-#             dataList.popleft()
-                            
-            
-#             if(dataList[0]=='+' and dataList[1]=='$'):
-#                 try:
-#                     adc_reading = int(dataList[2],16)*16 + int(dataList[3],16)
-#                     angle_reading = int(dataList[4],16)*16 + int(dataList[5],16)
-#                     angle_theta = (angle_reading-21)*(180/(75-21))
-#                     sig.go(adc_reading, angle_reading)                    
-#                 except ValueError, e:
-#                     pass
-                    
-
-#         except serial.SerialException, e:
-#             print("Serial Thread Exception:", e)
-
-
-# def openTelemetryThread(port_name, baud_rate, alert_function):
-#     """
-#     Opens a thread with the given arguments that monitors serial comms and the window to draw to
-#     """
-#     try:
-#         #serial
-#         serial_obj=serial.Serial(
-#                  port=port_name,
-#                  baudrate=baud_rate,           #baudrate
-#                  bytesize=serial.EIGHTBITS,    #number of databits
-#                  parity=serial.PARITY_NONE,    #enable parity checking
-#                  stopbits=serial.STOPBITS_ONE, #number of stopbits
-#                  timeout=None,                 #set a timeout value, None for waiting forever
-#                  xonxoff=0,                    #enable software flow control
-#                  rtscts=0,                     #enable RTS/CTS flow control
-#                  )
-        
-#         serial_obj.close()
-#         serial_obj.open()
-#         serial_obj.flushInput()
-#         serial_obj.flushOutput()
-#         #thread
-#         telemetry_thread = threading.Thread(target = serialMonitor, name = "SerialMonitorThread", args=(serial_obj,alert_function))
-
-#         #telemetry_thread.setDaemon(1)    #close with application
-
-#         telemetry_thread.start()
-        
-#         return telemetry_thread 
-
-#     except serial.SerialException, e:
-#         print("Could not open port %s: %s" % (port_name, e))
-#     except RuntimeError, e:
-#         print("Runtime error: %s" (e))
-
-
-
-# def error_calc(reference, secondary):
-#     """
-#     Accepts 2 slices of an array and it's time-shifted rotation.
-#     These arrays must be the same length
-#     """
-#     error = 0
-#     for i in range(len(reference)):
-#         print "\t",reference
-#         print "\t",secondary
-#         print "\t-"*5
-#         error += abs(reference[i] - secondary[i])
-
-
-#     return error
-
-
-# def find_angle(reference, secondary):
-#     """
-#     Finds the best guess for rotation based on the error, then returns the guessed angle we rotated.
-#     """
-    
-#     #these 2 must be even numbers th
-#     window_width    =   16               #width of search space. larger = slower, more accurate
-#     offset_width    =   20              #amount of side-to-side oscillation. lower = faster, less accurate
-    
-#     center          =   26              #center of rotation
-    
-#     rad_inc         =   math.pi / 54    #used for determining angles
-#     angle_errors    =   {}              #map offset_angle => error
-
-#     #setup reference window
-#     ref_start   =   center - (window_width/2)
-#     ref_end     =   ref_start + window_width
-#     ref_window  =   reference[ref_start:ref_end]        #reference window to use
-
-#     #setup loop
-#     smallest_error   =   255                              #keep track of the smallest error we see
-#     closest_angle   =   0                              #track the biggest error's angle
-#     nearest_offset  =   0
-
-#     for o in range(offset_width):
-#         relative_offset     =   (o-offset_width/2)+1        #offset, relative to "center" to use, in array indices
-#         angle               =   (rad_inc * 180/math.pi) * relative_offset #convert the index offset to degrees
-        
-#         secondary_start     =   center  + relative_offset - window_width/2 #- (offset_width/2) + o
-
-#         print secondary_start, secondary[secondary_start], relative_offset
-#         secondary_end       =   secondary_start + window_width
-#         secondary_window    =   secondary[secondary_start:secondary_end]
-
-#         angle_errors[str(angle)] = error_calc(ref_window, secondary_window)
-#         if angle_errors[str(angle)] < smallest_error:
-#             smallest_error   =   angle_errors[str(angle)]
-#             closest_angle   =   angle
-#             nearest_offset  =   relative_offset
-             
-
-#     print angle_errors
-    
-
-
-#     return "Angle: ", closest_angle, "Offset: ", nearest_offset
-
-
-
-# def generate_test_readings(shift_indices, size=80):
-#     """
-#     Returns a pair of readings. One is shifted and noisy
-#     """
-#     shift_size  =   -shift_indices           
-#     start       =   numpy.random.random_integers(80, 160,  size=(size,))    #simulated measurement
-
-#     start[40]   =   27      #simulated 'spike' to help track on chart
-
-#     end         =   (start[:]+numpy.random.randint(-10,10, size=(size,)))   #simulated noisey second measurement
-
-#     readings    =   start[10:-10]                                           #take slices
-#     readings2   =   end[10+shift_size:-10+shift_size]                       #take shifted slices
-
-#     return readings.tolist(), readings2.tolist()
-
-
-# def serialGenerate(serial_obj):
-#     """
-#     The generator thread
-#     """
-#     global isAlive
-    
-#     rad_inc = math.pi / 54
-#     loops   = 2
-#     l= 0
-#     while isAlive:
-#         try:
-
-#             readings, readings2 = generate_test_readings(-9)
-                     
-            
-#             while l < loops:
-#                 count_inc   =   0
-#                 radians = 0.0
-
-#                 while count_inc  < 54:
-#                     time.sleep(0.02)
-#                     for character in "+$":
-#                         serial_obj.write(character)
-                    
-#                     radians += rad_inc;
-#                     if radians > (2 * math.pi):
-#                         radians = 0.0
-                                    
-#                     #run twice
-#                     if l == 0:
-#                         some_num = int(readings[count_inc])#int(random.randint(0, 5) + 120*(1 + math.sin(radians)))
-#                     else:
-#                         some_num = int(readings2[count_inc])#int(random.randint(0, 5) + 120*(1 + math.sin(radians)))
-
-
-#                     serial_obj.write(num2hex(some_num))
-#                     serial_obj.write(num2hex(count_inc))
-
-#                     count_inc += 1
-                
-#                 l += 1            
-            
-#             isAlive = 0
-
-#             print find_angle(readings, readings2)
-
-#             break;
-
-#         except exceptions.AttributeError, e:
-#             pass    #most likely came during interpreter shutdown 
-#             #raise e               
-#         except serial.SerialException, e:
-#             print("Serial Thread Exception:", e)
-#         except serial.SerialTimeoutException, e:
-#             print("Serial Thread Timeout Exception:", e)
-           
-
-# def openGeneratorThread(port_name, baud_rate):
-#     """
-#     Opens a thread with the given arguments that creates serial comms
-#     """
-#     try:
-#         #serial
-#         serial_obj=serial.Serial(
-#                  port=port_name,
-#                  baudrate=baud_rate,        #baudrate
-#                  bytesize=serial.EIGHTBITS,    #number of databits
-#                  parity=serial.PARITY_NONE,    #enable parity checking
-#                  stopbits=serial.STOPBITS_ONE, #number of stopbits
-#                  timeout=None,             #set a timeout value, None for waiting forever
-#                  xonxoff=0,             #enable software flow control
-#                  rtscts=0,              #enable RTS/CTS flow control
-#                  )
-#         serial_obj.close()                 
-#         serial_obj.open()
-#         serial_obj.flushInput()
-#         serial_obj.flushOutput()
-#         #thread
-#         generator_thread = threading.Thread(target = serialGenerate, name = "SerialGenerateThread",args=(serial_obj,))
-
-#         #generator_thread.setDaemon(1)    #close with application
-
-#         generator_thread.start()
-#         return generator_thread        
-        
-#     except exceptions.AttributeError, e:
-#         pass    #most likely came during interpreter shutdown  
-#     except serial.SerialException, e:
-#         print("Could not open port %s: %s" (port_name, e))
-#     except RuntimeError, e:
-#         print("Runtime error: %s" (e))
 
 
 @QtCore.pyqtSlot(int,int)
@@ -299,9 +36,9 @@ def make():
     Setup the GUI, and plots
     """
     global plot_update_signal_wrappers
-    
+
+    #setup strip chart    
     stripWidget = Qt.QWidget()
-   
     
     stripChart = StripChart(stripWidget)
     stripChart.setTitle("Range vs Time")
@@ -309,19 +46,19 @@ def make():
 
     stripLayout = Qt.QVBoxLayout(stripWidget)
     stripLayout.addWidget(stripChart)
-   
 
+    #setup polar plot
     polarWidget = Qt.QWidget()
    
-
     polarPlot = PolarPlot(polarWidget)
-
     polarLayout = Qt.QVBoxLayout(polarWidget)
     polarLayout.addWidget(polarPlot)
     
+    #set sizes
     stripWidget.resize(600, 400)
     polarWidget.resize(600,600)
 
+    #setup main window
     windowWidget = Qt.QWidget()
     windowLayout = Qt.QVBoxLayout()
     windowWidget.setLayout(windowLayout)
@@ -342,8 +79,11 @@ def stop_threads():
     """
     Stop threads from running
     """
+    
+    #send threads kill signal
     setAlive(0)
 
+    #wait for them to halt
     try:
         g_thread.join()
         t_thread.join()
@@ -356,8 +96,6 @@ def main(args):
     Setup threads and serial 
     """
     
-    global isAlive
-    global telemetryPause
     global t_thread, g_thread
     
     print("Running")
@@ -367,12 +105,14 @@ def main(args):
     app         = Qt.QApplication(args)
     port_names  =    enumerateSerialPorts()
     strip,polar,window = make()
-    app.aboutToQuit.connect(stop_threads)
-
+    
     #signal to threads 
     setAlive(1)
     t_thread    = openTelemetryThread("COM6",38400, alert_function=updatePlots)
     g_thread    = openGeneratorThread('COM9',38400)
+    
+    #detect when we're shutting down and send threads the kill signal
+    app.aboutToQuit.connect(stop_threads)    
     
     #close when qtapp closes
     sys.exit(app.exec_())  
--- util_functions.py
+++ util_functions.py
@@ -23,7 +23,7 @@ class SigWrapper(QtCore.QObject):
 
 def num2hex(some_val):
     """
-    Converts an integer number to a 2-digit hex representation
+    Converts an 8-bit integer number to a 2-digit hex representation
     """
     the_hex = ""
     if some_val < 16 :
@@ -47,15 +47,9 @@ def rangeConvert(measurement, type = 0, units = "cm"):
         
         if measurement > 53: 
             #use calculation 1 for 85-150cm
-            #output = 10000 /((256/3.3) * (measurement*3.0/256) + 42) - 20.42
-            #output = 10000 /(measurement + 42) - 20.42
-            ##  output = (1/.00001)/(measurement + (0.0042/0.0001))-20.42
             output = 10000 /(measurement + 42) - 20.42
         else:
             #use calculation 2 for 20-84 cm
-            #output = 16667 /((256/3.3) * (measurement*3.0/256) + 128.33) - 5.42
-            #output = 16667 /(measurement + 128.33) - 5.42
-            ##  output = (1/0.00006)/(measurement + (0.0077/0.00006)) - 5.42
             output = 16667 /(measurement + 128.33) - 5.42
     
         if output > 150:
@@ -65,92 +59,12 @@ def rangeConvert(measurement, type = 0, units = "cm"):
     
     #type is short range sensor
     elif type == 1: 
-        #output = 1111.11 /((256/3.3) * (measurement*3.0/256) + 4.56) - 1
-        #output = 1111.11 /(measurement + 4.56) - 1
-        ##  output = (1/.00009)/(measurement + (0.0041/0.0009))-1
         output = 1111.11 /(measurement + 4.56) - 1
         if output > 40:
             output = 40
         elif output < 1:
             output = 0
-    
-    # type is accelerometer X axis
-    elif type == 2:
-        if xInit ==  0:
-            prevX = measurement
-            xInit = 1
-            
-        if measurement == 0 and prevX == 255:
-            xParse = prevX + 1
-        elif measurement == 255 and prevX == 0:
-            xParse = xParse - 1
-        elif measurement > prevX:
-            xParse = xParse + 1
-        elif measurement < prevX:
-            xParse = xParse - 1
-            
-        prevX = measurement
-        output = xParse
-        # output("Parsed X: %s" % str(output))
-    
-    # type is accelerometer Y axis
-    elif type == 3:
-        if yInit ==  0:
-            prevY = measurement
-            yInit = 1
-            
-        if measurement == 0 and prevY == 255:
-            yParse = prevY + 1
-        elif measurement == 255 and prevY == 0:
-            yParse = yParse - 1
-        elif measurement > prevY:
-            yParse = yParse + 1
-        elif measurement < prevY:
-            yParse = yParse - 1
-            
-        prevY = measurement
-        output = yParse
-        # output("Parsed Y: %s" % str(output))
-    
-    # type is accelerometer Z axis
-    elif type == 4:
-        if zInit ==  0:
-            prevZ = measurement
-            zInit = 1
-            
-        if measurement == 0 and prevZ == 255:
-            zParse = prevZ + 1
-        elif measurement == 255 and prevZ == 0:
-            zParse = zParse - 1
-        elif measurement > prevZ:
-            zParse = zParse + 1
-        elif measurement < prevZ:
-            zParse = zParse - 1
-            
-        prevZ = measurement
-        output = zParse
-        # output("Parsed Z: %s" % str(output))
-    
-    #capacitor voltage
-    elif type == 5:
-        if(measurement==0):
-            return 0
-        else:   
-            output =  ((3.3*measurement)/256.0 * 4) * 10
-            return round(output, 0)
 
-    #solar voltage
-    elif type == 6:
-        if(measurement==0):
-            return 0
-        else:   
-            output =  ((3.3*measurement)/256.0 * 4) * 10
-            return round(output, 0)
-    else:
-        pass
-
-    #output("Calculated range to be: %d" % math.floor(output))
-    
     if units == "in":
         output = output / 2.54
     
